98ef22f030b0c32eb89a608bc2ea76c249222c2e
Ashfaque Hossain Abir <108783774+abirett5@users.noreply.github.com>
2024-06-30 17:51:56 +0600

    Uploaded

---
+++ script.js
@@ -0,0 +1,31 @@
+// JSON = (JavaScript Object Notation) data-interchange format 
+//                Used for exchanging data between a server and a web application
+//                JSON files {key:value} OR [value1, value2, value3]
+
+// JSON.stringify() = converts a JS object to a JSON string.
+// JSON.parse() = converts a JSON string to a JS object
+
+
+const display = document.getElementById("display");
+
+
+// --> JSON Stringify
+
+// const jsonNames = `["Spongebob", "Patrick", "Squidward", "Sandy"]`;
+// const jsonPerson = `{"name": "Spongebob","age": 30,"isEmployed": true,"hobbies": ["Jellyfishing", "Karate", "Cooking"]}`;
+// const jsonPeople = `[{"name": "Spongebob","age": 30,"isEmployed": true},
+//                     {"name": "Patrick","age": 34,"isEmployed": false},
+//                     {"name": "Squidward","age": 50,"isEmployed": true},
+//                     {"name": "Sandy","age": 27,"isEmployed": false}]`;
+
+// const jsonString = JSON.stringify(people);
+
+// const parsedData = JSON.parse(jsonPerson);
+
+// console.log(parsedData);
+
+
+fetch("/json-project/people.json")
+    .then(response => response.json())
+    .then(values => values.forEach(value => console.log(value.name)))
+    .catch(error => console.error(error));
